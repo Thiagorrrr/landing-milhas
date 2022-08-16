41627be4a9869ce720798b956c7cc51d8c134a09
Thiago <Thiago.PereiradaSilva-ext@ab-Inbev.com>
2022-08-16 17:51:42 -0300

    new components

--- src/components/atoms/CardUser/styles.ts
+++ src/components/atoms/CardUser/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
-    width: 80%;
+    width: 95%;
     max-width: 320px;
     display: inline-block;
     border-radius: 20px;
