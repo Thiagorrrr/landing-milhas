7e46750c080fd23991c56e4ecf663713a48da3f2
Thiago <Thiago.PereiradaSilva-ext@ab-Inbev.com>
2022-08-19 13:40:41 -0300

    logo link

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -2,5 +2,7 @@ import React from 'react'
 import * as S from './styles'
 
 export const Logo = () => (
-  <S.LogoWrapper src="img/logo.png" alt="logo milhas no alvo" />
+  <S.link href="/">
+    <S.LogoWrapper src="img/logo.png" alt="logo milhas no alvo" />
+  </S.link>
 )
--- src/components/atoms/Logo/styles.ts
+++ src/components/atoms/Logo/styles.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 import media from 'styled-media-query'
 
+export const link = styled.a`
+  z-index: 20;
+`
 export const LogoWrapper = styled.img`
   width: 11.8rem;
   z-index: 20;
-
-  ${media.greaterThan('medium')`
-    width: 38rem;
-  `}
 `
