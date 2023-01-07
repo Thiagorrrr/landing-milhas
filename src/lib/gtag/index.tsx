3c27d26b4a68720e8cd3bbcb889767e101b995e9
Thiago Pereira Silva <thiago.pereiradasilva-ext@ab-inbev.com>
2023-01-07 17:26:43 -0300

    analytics

--- src/lib/gtag/index.tsx
+++ src/lib/gtag/index.tsx
@@ -3,13 +3,13 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 declare global {
   interface Window {
-    gtag: any
+    gtag?: any
   }
 }
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 
 export const pageview = (url: any) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  ;(window as any).gtag('config', GA_TRACKING_ID, {
     page_path: url
   })
 }
@@ -22,7 +22,7 @@ type ValuesProp = {
   value: any
 }
 export const event = ({ action, category, label, value }: ValuesProp) => {
-  window.gtag('event', action, {
+  ;(window as any).gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value
